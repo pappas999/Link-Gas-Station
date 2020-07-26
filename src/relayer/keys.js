a06d216cce7df2c07fe672ad2fd4ddb4bade11e1
pappas999 <harry@genesisblockchain.com.au>
2020-07-26 22:56:54 +0930

    removed keys
    removed keys

--- src/relayer/keys.js
+++ src/relayer/keys.js
@@ -1,7 +1,7 @@
 let keys = {};
 keys.keys = {
-    public: "0xaF9aA280435E8C13cf8ebE1827CBB402CE65bBf7",
-	private: "fa7ed70c2c43cbdad58ad80a9a5814f09f0669e202579d8e316dbe9523f99cec"
+    public: "",
+	private: ""
 }
 module.exports = {keys}
 
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+ const HDWalletProvider = require('truffle-hdwallet-provider');
+ const infuraKey = "";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const mnemonic = "";
 
 module.exports = {
   /**
@@ -60,14 +60,15 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+     ropsten: {
+       provider: new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+       network_id: 3,       // Ropsten's id
+       gas: 8000000,        // Ropsten has a lower block limit than mainnet
+	   gasPrice: 103000000000,
+       confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+       timeoutBlocks: 500,  // # of blocks before a deployment times out  (minimum/default: 50)
+       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+     }
 
     // Useful for private networks
     // private: {
@@ -85,15 +86,15 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-       version: "0.4.24"    // Fetch exact version from solc-bin (default: truffle's version)
+       version: "0.5.13",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+       settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
       //    enabled: false,
       //    runs: 200
       //  },
-      //  evmVersion: "byzantium"
-      // }
+        evmVersion: "istanbul"
+       }
     }
   }
 }
